9968b1457a98754eb1e6aea95a3f9637dd30850f
john.njau <john.njoroge@student.moringaschool.com>
2022-07-01 20:36:43 +0300

    init files

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+new Vue({
+  router,
+  store,
+  axios,
+  render: h => h(App)
+}).$mount('#app')
