af3c7b6c0882323d3ceb132ec1270eea8cc8741f
john.njau <john.njoroge@student.moringaschool.com>
2022-07-03 16:03:38 +0300

    feature: login template

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -1,96 +1,119 @@
 <template>
   <!-- Section: Design Block -->
   <section class="background-radial-gradient overflow-hidden" id="login-page">
-    <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-      <div class="row gx-lg-5 align-items-center mb-5">
-        <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+    <div
+      class="container-fluid py-5 px-md-5 d-flex text-center text-lg-start my-5"
+    >
+      <div class="row gx-lg-5 align-items-center justify-content-center mb-5">
+        <div
+          class="col-lg-7 mb-5 mb-lg-0"
+          style="z-index: 10"
+          id="digital-content"
+        >
           <h1
             class="my-5 display-5 fw-bold ls-tight"
             style="color: hsl(218, 81%, 95%)"
           >
             <h3>
-              <strong><small>Digital Gallery:</small></strong>
+              <strong style="font-size: 40px">Digital Gallery:</strong>
             </h3>
             The best offer <br />
-            <span style="color: hsl(218, 81%, 75%)">for your business</span>
+            <span>for your business</span>
           </h1>
-          <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+          <p
+            class="mb-4 opacity-70 text-white"
+            style="color: hsl(218, 81%, 85%); font-size: 20px"
+          >
             Lorem ipsum dolor, sit amet consectetur adipisicing elit.
             Temporibus, expedita iusto veniam atque, magni tempora mollitia
             dolorum consequatur nulla, neque debitis eos reprehenderit quasi ab
             ipsum nisi dolorem modi. Quos?
           </p>
+          <button
+            class="btn btn-block btn-sm outline-2"
+            style="background-color: #f8fa29; width: 30%; height: 40px"
+          >
+            READ MORE
+          </button>
         </div>
 
-        <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
-          <div
-            id="radius-shape-1"
-            class="position-absolute rounded-circle shadow-5-strong"
-          ></div>
-          <div
-            id="radius-shape-2"
-            class="position-absolute shadow-5-strong"
-          ></div>
-
-          <div class="card bg-glass">
-            <div class="card-body px-4 py-5 px-md-5">
-                <div class="brand">
-                    <a type="button" class="btn btn-rounded" style="background-color:#F8FA29; border-radius:10px "> <strong>mookh.</strong> </a>
-                </div>
-                <br>
-                <div class="buyer">
-                    <h4><strong><i class="fa-solid fa-user"></i> Buyer Log In</strong></h4>
-                    <p>Don't have an account? <a href="/signup">sign up</a></p>
-                </div>
-                <br><br>
-              <form>
+        <div class="col-lg-4 mb-5 mb-lg-0 position-relative">
+          <div class="card" id="login-card" style="">
+            <div class="card-body py-5 px-md-5">
+              <div class="brand">
+                <a
+                  type="button"
+                  class="btn btn-rounded text-left"
+                  style="
+                    background-color: #f8fa29;
+                    border-radius: 20px;
+                    width: 40%;
+                  "
+                >
+                  <strong>mookh.</strong>
+                </a>
+              </div>
+              <br />
+              <div class="buyer text-white">
+                <h4>
+                  <strong><i class="fa-solid fa-user"></i> Buyer Log In</strong>
+                </h4>
+                <p>Don't have an account? <a href="/signup">sign up</a></p>
+              </div>
+              <br /><br />
+              <form @submit.prevent="SubmitForm">
                 <!-- 2 column grid layout with text inputs for the first and last names -->
 
                 <!-- Email input -->
                 <div class="form-outline mb-4">
-                    <label class="form-label" for="form3Example3"
-                    >Email address</label
-                  >
-                  <input type="email" id="form3Example3" class="form-control" />
-                  
+                  <label class="form-label text-white" for="form3Example3">
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    id="form3Example3"
+                    class="form-control"
+                    v-model="email"
+                    placeholder="email"
+                  />
                 </div>
 
                 <!-- Password input -->
                 <div class="form-outline mb-4">
-                <label class="form-label" for="form3Example4">Password</label>
+                  <label class="form-label text-white" for="form3Example4"
+                    >Password</label
+                  >
 
                   <input
                     type="password"
-                    id="form3Example4"
                     class="form-control"
+                    v-model="password"
+                    placeholder="password"
                   />
                 </div>
-
-                
-
+                <div class="notification text-danger" v-if="errors.length">
+                  <p v-for="error in errors" :key="error">{{ error }}</p>
+                </div>
                 <!-- Submit button -->
-                <div class="form-outline mb-4 form-group">
-                <button
-                  type="submit"
-                  class="btn btn-block mb-4 btn-sm outline-2"
-                  style="background-color:#F8FA29;"
-                >
-                  Log In
-                </button>
+                <div class="form-outline mb-4 form-group" id="login">
+                  <button
+                    class="btn btn-block btn-sm outline-2"
+                    style="background-color: #f8fa29; width: 100%; height: 40px"
+                  >
+                    LOG IN
+                  </button>
                 </div>
                 <!-- Checkbox -->
-                <div class="form-check mb-5">
+                <div class="form-check mb-5 p-0">
                   <label class="form-check-label" for="form2Example33">
-                    <a href="">
-                        Forgot your password?
+                    <a href="/reset-password" class="text-secondary">
+                      Forgot your password?
                     </a>
                   </label>
                 </div>
-
-                <!-- Register buttons -->
-                <div class="text-center">
+                <div class="text-white p-0">
                   By login in to your account, you agree to our
-                  <a href="">Terms and Conditions</a>.
+                  <a href="" class="text-white">Terms and Conditions</a>.
                 </div>
               </form>
             </div>
@@ -102,14 +125,88 @@
   <!-- Section: Design Block -->
 </template>
 <script>
+import axios from "axios";
+
 export default {
-  data() {},
+  data() {
+    return {
+      email: "",
+      password: "",
+      errors: [],
+    };
+  },
+  mounted() {
+    document.title = "Login";
+  },
+
+  methods: {
+    async SubmitForm() {
+      axios.defaults.headers.common["Authorization"] = "";
+
+      localStorage.removeItem("token");
+
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      await axios
+        .post("/api/v1/token/login", formData)
+        .then((response) => {
+          const token = response.data.auth_token;
+
+          this.$store.commit("setToken", token);
+
+          axios.defaults.headers.common["Authorization"] = "Token" + token;
+
+          localStorage.setItem("token", token);
+
+          const toPath = this.$route.query.to || "/";
+
+          this.$router.push(toPath);
+        })
+
+        .catch((error) => {
+          if (error.response) {
+            for (const property in error.response.data) {
+              this.errors.push(`${property}: ${error.response.data[property]}`);
+            }
+          } else {
+            this.errors.push(error.message);
+
+            console.log(JSON.stringify(error));
+          }
+        });
+    },
+  },
 };
 </script>
+
+
   <style scoped>
+#login {
+  border: 1px solid #ffffff;
+  /* box-shadow: 2px 2px 2px 2px rgba(255, 255, 255); */
+}
+
+#login-page {
+  height: 100vh;
+}
+
+#login-card {
+  background-color: #000101;
+  border-radius: 15px;
+}
+
 .background-radial-gradient {
   background-color: hsl(218, 41%, 15%);
-  background-image: radial-gradient(
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.5)),
+    url("../../assets/login-image.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  opacity: 0.9;
+  /* background-image: radial-gradient(
       650px circle at 0% 0%,
       hsl(218, 41%, 35%) 15%,
       hsl(218, 41%, 30%) 35%,
@@ -124,30 +221,8 @@ export default {
       hsl(218, 41%, 20%) 75%,
       hsl(218, 41%, 19%) 80%,
       transparent 100%
-    );
+    ); */
 }
 
-#radius-shape-1 {
-  height: 220px;
-  width: 220px;
-  top: -60px;
-  left: -130px;
-  background: radial-gradient(#44006b, #ad1fff);
-  overflow: hidden;
-}
 
-#radius-shape-2 {
-  border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
-  bottom: -60px;
-  right: -110px;
-  width: 300px;
-  height: 300px;
-  background: radial-gradient(#44006b, #ad1fff);
-  overflow: hidden;
-}
-
-.bg-glass {
-  background-color: hsla(0, 0%, 100%, 0.9) !important;
-  backdrop-filter: saturate(200%) blur(25px);
-}
 </style>
