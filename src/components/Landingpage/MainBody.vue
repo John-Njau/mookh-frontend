04873678f630e475d9f8f205c79de7bd805a8972
john.njau <john.njoroge@student.moringaschool.com>
2022-07-06 16:00:47 +0300

    fix: image display

--- src/components/Landingpage/MainBody.vue
+++ src/components/Landingpage/MainBody.vue
@@ -122,7 +122,7 @@
                     <img
                       class="card-img-top"
                       style="height: 380px"
-                      :src="'http://127.0.0.1:8000'+event.profile_pic"
+                      :src="'http://127.0.0.1:8000' + event.profile_pic"
                       alt="Card image cap"
                     />
                   </div>
