89ab2ce393685a17a185863929a710afaf4f7365
victoria mwangi <victoriamwangi021@gmail.com>
2022-07-11 11:33:26 +0300

    carousel style

--- src/views/Profile/slider.vue
+++ src/views/Profile/slider.vue
@@ -1,187 +1,83 @@
 <template>
   <div>
-    <div class="container my-5">
-      <div
-        id="multi-item-example"
-        class="carousel slide carousel-multi-item"
-        data-bs-ride="carousel"
-      >
-        <!--Slides-->
-
-        <div>
-          <carousel
-            v-if="
-              detIncidente && detIncidente.fotos && detIncidente.fotos.length
-            "
-            :autoplay="true"
-            :loop="true"
-            :center="true"
-            :nav="false"
-            :margin="5"
-          >
-            <img
-              v-for="item in detIncidente.fotos"
-              :key="item.fecha"
-              :src="item.path"
-              width="446"
-              height="446"
-              @click="showSingle(item.path)"
-            />
-          </carousel>
-        </div>
-        <!--/.Slides-->
-      </div>
-      <!--/.Carousel Wrapper-->
-    </div>
+    <agile
+      :nav-buttons="false"
+      :autoplay-speed="5000"
+      :speed="2500"
+      fade
+      pause-on-hover
+      pause-on-dots-hover
+      autoplay
+      id="try"
+    >
+      <img
+        class="slide"
+        src="https://images.unsplash.com/photo-1509549649946-f1b6276d4f35?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      />
+      <img
+        class="slide"
+        src="https://images.unsplash.com/photo-1511469054436-c7dedf24c66b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9"
+      />
+      <img
+        class="slide"
+        src="https://images.unsplash.com/photo-1511135232973-c3ee80040060?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9"
+      />
+      <img
+        class="slide"
+        src="https://images.unsplash.com/photo-1511231683436-44735d14c11c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9"
+      />
+      <img
+        class="slide"
+        src="https://images.unsplash.com/photo-1517677129300-07b130802f46?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9"
+      />
+    </agile>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+Vue.use(VueAgile);
 
-export default {
-  name: "BodyCont",
-
-  data() {
-    return {
-      // tasks
-      events: [],
-    };
+app = new Vue({
+  el: "#try",
+  components: {
+    agile: VueAgile,
   },
-  methods: {
-    getData() {
-      axios
-        .get("/api/events/")
-        .then((response) => {
-          this.events = response.data;
-        })
-        .catch((error) => {
-          // log the error
-          console.log(error);
-        });
-    },
-  },
-  mounted() {
-    // Fetch tasks on page load
-    this.getData();
-  },
-};
+});
 </script>
 
-<style scoped>
-.custom-btn {
-  width: 200px;
-  height: 40px;
-  color: black;
-  border-radius: 5px;
-  padding: 10px 25px;
-  font-family: "Lato", sans-serif;
-  font-weight: 500;
-  background: transparent;
-  cursor: pointer;
-  transition: all 0.3s ease;
-  position: relative;
-  display: inline-block;
-  box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
-    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
-  outline: none;
-}
+<style scoped lang='sass'>
+.agile
+  &__dots
+    bottom: 10px
+    flex-direction: column
+    right: 30px
+    position: absolute
 
-/* 3 */
-.btn-3 {
-  background: #ffff00;
-  background: linear-gradient(
-    0deg,
-    rgba(255, 255, 0, 1) 0%,
-    rgba(255, 255, 0) 100%
-  );
-  width: 130px;
-  height: 40px;
-  line-height: 42px;
-  padding: 0;
-  border: none;
-}
-.btn-3 span {
-  position: relative;
-  display: block;
-  width: 100%;
-  height: 100%;
-}
-.btn-3:before,
-.btn-3:after {
-  position: absolute;
-  content: "";
-  right: 0;
-  top: 0;
-  background: rgba(255, 255, 0, 1);
-  transition: all 0.3s ease;
-}
-.btn-3:before {
-  height: 0%;
-  width: 2px;
-}
-.btn-3:after {
-  width: 0%;
-  height: 2px;
-}
-.btn-3:hover {
-  background: transparent;
-  box-shadow: none;
-}
-.btn-3:hover:before {
-  height: 100%;
-}
-.btn-3:hover:after {
-  width: 100%;
-}
-.btn-3 span:hover {
-  color: black;
-}
-.btn-3 span:before,
-.btn-3 span:after {
-  position: absolute;
-  content: "";
-  left: 0;
-  bottom: 0;
-  background: black;
-  transition: all 0.3s ease;
-}
-.btn-3 span:before {
-  width: 2px;
-  height: 0%;
-}
-.btn-3 span:after {
-  width: 0%;
-  height: 2px;
-}
-.btn-3 span:hover:before {
-  height: 100%;
-}
-.btn-3 span:hover:after {
-  width: 100%;
-}
+  &__dot
+    margin: 5px 0
 
-.small {
-  /*border: 1px solid black;*/
-  box-shadow: 2px 1px 2px 1px rgba(0, 0, 0, 0.2);
-  border-radius: 5px;
-  margin-left: 30px;
-  padding-left: 0px;
-  padding-right: 0px;
-}
+    button
+      background-color: transparent
+      border: 1px solid #fff
+      cursor: pointer
+      display: block
+      height: 10px
+      font-size: 0
+      line-height: 0
+      margin: 0
+      padding: 0
+      transition-duration: .3s
+      width: 10px
 
-body {
-  background-color: #f8f8ff;
-}
+    &--current,
+    &:hover
+      button
+        background-color: #fff
 
-.container {
-  color: black;
-}
-
-.btn {
-  border: 1px solid black;
-  border-radius: 0px;
-  width: 200px;
-  background-color: #ffff00;
-}
+// Slides styles
+.slide
+  display: block
+  height: 500px
+  object-fit: cover
+  width: 100%
 </style>
